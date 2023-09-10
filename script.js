0cbde11c935b3d4ee8bb2f562b45330100735fc7
Joe <8309575+josephkreydt@users.noreply.github.com>
2023-09-10 08:22:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,666 @@
+var mediaJSON = `{
+	"winter": {
+		"book": [{
+				"title": "Misery",
+				"author": "Stephen King"
+			},
+			{
+				"title": "True Grit",
+				"author": "Charles Portis"
+			},
+			{
+				"title": "The Bear and the Nightingale",
+				"author": "Katherine Arden"
+			},
+			{
+				"title": "Catcher in the Rye",
+				"author": "JD Salinger"
+			},
+			{
+				"title": "Snow Child",
+				"author": "Eowyn Ivey"
+			},
+			{
+				"title": "To the Bright Edge of the World",
+				"author": "Eowyn Ivey"
+			},
+			{
+				"title": "Brian's Winter",
+				"author": "Gary Paulsen"
+			},
+			{
+				"title": "Snow Country",
+				"author": "Yasunari Kawabata"
+			}
+		],
+		"film": [{
+				"title": "Reindeer Games"
+			},
+			{
+				"title": "The Bourne Identity"
+			},
+			{
+				"title": "Zach and Miri"
+			},
+			{
+				"title": "The Hateful Eight"
+			},
+			{
+				"title": "It Happened on Fifth Avenue"
+			},
+			{
+				"title": "Christmas Vacation"
+			},
+			{
+				"title": "On Her Majesty's Secret Service (James Bond)"
+			},
+			{
+				"title": "Trading Places"
+			},
+			{
+				"title": "Mr Deeds"
+			},
+			{
+				"title": "Dumb and Dumber"
+			},
+			{
+				"title": "Balto"
+			},
+			{
+				"title": "The Mighty Ducks"
+			},
+			{
+				"title": "101 Dalmations"
+			},
+			{
+				"title": "Surf's Up"
+			},
+			{
+				"title": "Happy Feet"
+			},
+			{
+				"title": "Out Cold"
+			},
+			{
+				"title": "Fargo"
+			},
+			{
+				"title": "The Chronicles of Narnia: The Lion, the Witch, and the Wardrobe"
+			},
+			{
+				"title": "Spectre (James Bond)"
+			},
+			{
+				"title": "Goldeneye (James Bond)"
+			},
+			{
+				"title": "Harry Potter (series)"
+			},
+			{
+				"title": "Lars and the Real Girl"
+			},
+			{
+				"title": "A Case of You"
+			},
+			{
+				"title": "The Hobbit (series)"
+			},
+			{
+				"title": "Lord of the Rings (series)"
+			},
+			{
+				"title": "Higher"
+			},
+			{
+				"title": "Deeper"
+			},
+			{
+				"title": "Further"
+			},
+			{
+				"title": "Eternal Sunshine of the Spotless Mind"
+			},
+			{
+				"title": "Beautiful Girls"
+			},
+			{
+				"title": "Journey to Greenland"
+			},
+			{
+				"title": "Under the Tuscan Sun"
+			},
+			{
+				"title": "Just Friends"
+			},
+			{
+				"title": "The Proposal"
+			},
+			{
+				"title": "The Shop Around the Corner"
+			},
+			{
+				"title": "12 Dates of Christmas"
+			},
+			{
+				"title": "Serendipity"
+			},
+			{
+				"title": "Rocky"
+			},
+			{
+				"title": "In Bruges"
+			},
+			{
+				"title": "Uncle Buck"
+			},
+			{
+				"title": "Scott Pilgrim vs the World"
+			},
+			{
+				"title": "Nick and Norah's Infinite Playlist"
+			},
+			{
+				"title": "Ice Age"
+			},
+			{
+				"title": "The Fountain"
+			},
+			{
+				"title": "Jeremiah Johnson"
+			},
+			{
+				"title": "The Fifth Estate"
+			},
+			{
+				"title": "South Park: Bigger, Longer & Uncut"
+			},
+      {
+        "title": "To All the Boys I've Loved Before"
+      }
+		]
+	},
+	"spring": {
+		"book": [{
+				"title": "1984",
+				"author": "George Orwell"
+			},
+			{
+				"title": "The Goldfinch",
+				"author": "Donna Tartt"
+			},
+			{
+				"title": "The Perks of Being a Wallflower",
+				"author": "Stephen Chbosky"
+			},
+			{
+				"title": "When We Were Saints",
+				"author": "Han Nolan"
+			},
+			{
+				"title": "The Life Changing Magic of Tidying Up",
+				"author": "Marie Kondo"
+			}
+		],
+		"film": [{
+				"title": "40 Days and 40 Nights"
+			},
+			{
+				"title": "The Boondock Saints"
+			},
+			{
+				"title": "Wedding Crashers"
+			},
+			{
+				"title": "Nick and Norah's Infinite Playlist"
+			},
+			{
+				"title": "Blue Valentine"
+			},
+			{
+				"title": "Valentine's Day"
+			},
+			{
+				"title": "Garden State"
+			},
+			{
+				"title": "Under the Tuscan Sun"
+			},
+			{
+				"title": "Midnight in Paris"
+			},
+			{
+				"title": "Napoleon Dynamite"
+			},
+			{
+				"title": "Paterson"
+			},
+      {
+        "title": "Empire Records"
+      }
+		]
+	},
+	"summer": {
+		"book": [{
+				"title": "The Sun Also Rises",
+				"author": "Ernest Hemingway"
+			},
+			{
+				"title": "A Salty Piece of Land",
+				"author": "Jimmy Buffett"
+			}
+		],
+		"film": [{
+				"title": "Love & Mercy"
+			},
+			{
+				"title": "Wedding Crashers"
+			},
+			{
+				"title": "Road House"
+			},
+			{
+				"title": "Mr Hobbs Takes a Vacation"
+			},
+			{
+				"title": "Flipper"
+			},
+			{
+				"title": "Fast & Furious (series)"
+			},
+			{
+				"title": "National Lampoon's Vacation"
+			},
+			{
+				"title": "Starsky & Hutch"
+			},
+			{
+				"title": "The Endless Summer"
+			},
+			{
+				"title": "Into the Blue"
+			},
+			{
+				"title": "Fool's Gold"
+			},
+			{
+				"title": "Rocket Power: Island of the Menehune"
+			},
+			{
+				"title": "The Wackness"
+			},
+			{
+				"title": "Jaws"
+			},
+			{
+				"title": "Begin Again"
+			},
+			{
+				"title": "The Hobbit: An Unexpected Journey"
+			},
+			{
+				"title": "A Good Year"
+			},
+			{
+				"title": "Moonrise Kingdom"
+			},
+			{
+				"title": "Papa Hemingway in Cuba"
+			},
+			{
+				"title": "Dragon"
+			},
+			{
+				"title": "The Big Boss"
+			},
+			{
+				"title": "The Life Aquatic with Steve Zissou"
+			},
+			{
+				"title": "Under the Tuscan Sun"
+			},
+			{
+				"title": "The Breakup"
+			},
+			{
+				"title": "Masterminds"
+			},
+			{
+				"title": "Chef"
+			},
+			{
+				"title": "Couples Retreat"
+			},
+			{
+				"title": "Waffle Street"
+			},
+			{
+				"title": "Paterson"
+			},
+			{
+				"title": "Eat Drink Man Woman"
+			},
+			{
+				"title": "Dirty Dancing"
+			}
+		]
+	},
+	"fall": {
+		"book": [{
+				"title": "Dracula",
+				"author": "Bram Stoker"
+			},
+			{
+				"title": "The Witches of Eastwick",
+				"author": "John Updike"
+			},
+			{
+				"title": "Staggerford",
+				"author": "Jon Hassler"
+			},
+			{
+				"title": "Dead Poets Society",
+				"author": "Nancy H Kleinbaum"
+			},
+			{
+				"title": "The October Country",
+				"author": "Ray Bradbury"
+			},
+			{
+				"title": "The Halloween Tree",
+				"author": "Ray Bradbury"
+			},
+			{
+				"title": "Something Wicked This Way Comes",
+				"author": "Ray Bradbury"
+			},
+			{
+				"title": "Silver Linings Playbook",
+				"author": "Matthew Quick"
+			},
+			{
+				"title": "It",
+				"author": "Stephen King"
+			},
+			{
+				"title": "Jane Eyre",
+				"author": "Charlotte Brontë"
+			},
+			{
+				"title": "The Gustav Sonata",
+				"author": "Rose Tremain"
+			},
+			{
+				"title": "Fahrenheit 451",
+				"author": "Ray Bradbury"
+			},
+			{
+				"title": "The Perks of Being a Wallflower",
+				"author": "Stephen Chbosky"
+			},
+			{
+				"title": "Snow Country",
+				"author": "Yasunari Kawabata"
+			}
+		],
+		"film": [{
+				"title": "The Karate Kid"
+			},
+			{
+				"title": "Dan in Real Life"
+			},
+			{
+				"title": "Road Trip"
+			},
+			{
+				"title": "Road House"
+			},
+			{
+				"title": "Big Daddy"
+			},
+			{
+				"title": "The Haunted Mansion"
+			},
+			{
+				"title": "Zombieland"
+			},
+			{
+				"title": "Ghostbusters"
+			},
+			{
+				"title": "Wallace & Gromit: The Curse of the Were-Rabbit"
+			},
+			{
+				"title": "Paranorman"
+			},
+			{
+				"title": "Hotel Transylvania"
+			},
+			{
+				"title": "Donnie Darko"
+			},
+			{
+				"title": "The Nightmare Before Christmas"
+			},
+			{
+				"title": "Goosebumps"
+			},
+			{
+				"title": "Casper"
+			},
+			{
+				"title": "Liberal Arts"
+			},
+			{
+				"title": "Stuck in Love"
+			},
+			{
+				"title": "Harry Potter (series)"
+			},
+			{
+				"title": "The Secret Life of Pets"
+			},
+			{
+				"title": "Paterson"
+			},
+			{
+				"title": "Dirty Dancing"
+			},
+			{
+				"title": "Planes, Trains & Automobiles"
+			},
+			{
+				"title": "It"
+			},
+			{
+				"title": "Moonrise Kingdom"
+			},
+			{
+				"title": "Nick and Norah's Infinite Playlist"
+			},
+			{
+				"title": "10 Cloverfield Lane"
+			},
+			{
+				"title": "The Proposal"
+			},
+			{
+				"title": "When Harry Met Sally"
+			},
+			{
+				"title": "Pocahontas"
+			},
+			{
+				"title": "The New Guy"
+			},
+			{
+				"title": "The Edge of Seventeen"
+			},
+			{
+				"title": "The Witches of Eastwick"
+			},
+			{
+				"title": "To All the Boys I've Loved Before"
+			},
+			{
+				"title": "Class Rank"
+			}
+		]
+	}
+}`
+
+var mediaObject = JSON.parse(mediaJSON);
+
+
+
+function generateList() {
+
+	function randomNumberForArray(sortedArrayOfMedia) {
+		var randomNumber = Math.floor(Math.random() * sortedArrayOfMedia.length);
+		return (randomNumber);
+	};
+
+	/*************** Winter Books ***************/
+	if (document.getElementById("winterCheckbox").checked == true && document.getElementById("bookCheckbox").checked == true) {
+
+		var winterBooksArray = mediaObject.winter.book;
+		var allWinterBooks = [];
+
+		for (var x in winterBooksArray) {
+			allWinterBooks.push(winterBooksArray[x].title + " by " + winterBooksArray[x].author);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			var randomNumber = randomNumberForArray(winterBooksArray);
+			document.getElementById("resultsDiv").innerHTML = allWinterBooks[randomNumber];
+
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allWinterBooks;
+		};
+
+		/*************** Winter Films ***************/
+	} else if (document.getElementById("winterCheckbox").checked == true && document.getElementById("filmCheckbox").checked == true) {
+		console.log("Winter and Film are checked.");
+
+		var winterFilmsArray = mediaObject.winter.film;
+		var allWinterFilms = [];
+
+		for (var x in winterFilmsArray) {
+			allWinterFilms.push(winterFilmsArray[x].title);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(winterFilmsArray);
+			document.getElementById("resultsDiv").innerHTML = allWinterFilms[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allWinterFilms;
+		};
+
+		/*************** Spring Books ***************/
+	} else if (document.getElementById("springCheckbox").checked == true && document.getElementById("bookCheckbox").checked == true) {
+		console.log("Spring and Book are checked.");
+
+		var springBooksArray = mediaObject.spring.book;
+		var allSpringBooks = [];
+
+		for (var x in springBooksArray) {
+			allSpringBooks.push(springBooksArray[x].title + " by " + springBooksArray[x].author);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(springBooksArray);
+			document.getElementById("resultsDiv").innerHTML = allSpringBooks[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allSpringBooks;
+		};
+
+		/*************** Spring Films ***************/
+	} else if (document.getElementById("springCheckbox").checked == true && document.getElementById("filmCheckbox").checked == true) {
+		console.log("Spring and Film are checked.");
+
+		var springFilmsArray = mediaObject.spring.film;
+		var allSpringFilms = [];
+
+		for (var x in springFilmsArray) {
+			allSpringFilms.push(springFilmsArray[x].title);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(springFilmsArray);
+			document.getElementById("resultsDiv").innerHTML = allSpringFilms[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allSpringFilms;
+		};
+
+		/*************** Summer Books ***************/
+	} else if (document.getElementById("summerCheckbox").checked == true && document.getElementById("bookCheckbox").checked == true) {
+		console.log("Summer and Book are checked.");
+
+		var summerBooksArray = mediaObject.summer.book;
+		var allSummerBooks = [];
+
+		for (var x in summerBooksArray) {
+			allSummerBooks.push(summerBooksArray[x].title + " by " + summerBooksArray[x].author);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(summerBooksArray);
+			document.getElementById("resultsDiv").innerHTML = allSummerBooks[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allSummerBooks;
+		};
+
+		/*************** Summer Films ***************/
+	} else if (document.getElementById("summerCheckbox").checked == true && document.getElementById("filmCheckbox").checked == true) {
+		console.log("Summer and Film are checked.");
+
+		var summerFilmsArray = mediaObject.summer.film;
+		var allSummerFilms = [];
+
+		for (var x in summerFilmsArray) {
+			allSummerFilms.push(summerFilmsArray[x].title);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(summerFilmsArray);
+			document.getElementById("resultsDiv").innerHTML = allSummerFilms[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allSummerFilms;
+		};
+
+		/*************** Fall Books ***************/
+	} else if (document.getElementById("fallCheckbox").checked == true && document.getElementById("bookCheckbox").checked == true) {
+		console.log("Fall and Book are checked.");
+
+		var fallBooksArray = mediaObject.fall.book
+		var allFallBooks = [];
+
+		for (var x in fallBooksArray) {
+			allFallBooks.push(fallBooksArray[x].title + " by " + fallBooksArray[x].author);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(fallBooksArray);
+			document.getElementById("resultsDiv").innerHTML = allFallBooks[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allFallBooks;
+		};
+
+		/*************** Fall Films ***************/
+	} else if (document.getElementById("fallCheckbox").checked == true && document.getElementById("filmCheckbox").checked == true) {
+		console.log("Fall and Film are checked.");
+
+		var fallFilmsArray = mediaObject.fall.film
+		var allFallFilms = [];
+
+		for (var x in fallFilmsArray) {
+			allFallFilms.push(fallFilmsArray[x].title);
+		};
+
+		if (document.getElementById("showOne").checked == true) {
+			randomNumber = randomNumberForArray(fallFilmsArray);
+			document.getElementById("resultsDiv").innerHTML = allFallFilms[randomNumber];
+		} else {
+			document.getElementById("resultsDiv").innerHTML = allFallFilms;
+		};
+	};
+};
